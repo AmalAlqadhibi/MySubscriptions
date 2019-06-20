6baecabcbdf5a4b37ec351ed0508e4f34960dc46
Amal Alqadhibi <amalqudaibi@gmail.com>
2019-06-20 16:19:26 +0300

    Initial Commit

---
+++ MySubscriptions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySubscriptions
+//
+//  Created by Amal Alqadhibi on 20/06/2019.
+//  Copyright © 2019 Amal Alqadhibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
